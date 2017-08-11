85751563daf18596060d3cecee9ba67e571ea4c4
Jeremy Gray <jeremygray001@gmail.com>
2017-08-10 21:11:44 -0600

    Updated articles table; added dummy data

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,3 @@ username varchar(60),
 profile_img varchar(255)
 );
 
-CREATE TABLE articles(
-  user_id varchar(55) not null,
-  url varchar(255),
-  rating int,
-  date_added timestamp,
-  bookmarked boolean
-);

52bf71a4236f82451b993b1fb3642da5e6ae6c73
Jeremy Gray <jeremygray001@gmail.com>
2017-08-13 18:19:14 -0600

    Hosting

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ app.get('/deletearticle/:uid/:id', articles.delete)
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(`${__dirname}/../build`))
+    app.use(express.static(`${__dirname}/../build/`))
     app.get('*', (req, res) => {
         res.sendFile(`${__dirname}/../build/index.html`)
     })

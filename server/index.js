7bee0bcdd690d9733f6a31b22ea020b0fdb97b04
Jeremy Gray <jeremygray001@gmail.com>
2017-08-14 00:20:09 -0600

    Hosting

--- server/index.js
+++ server/index.js
@@ -80,8 +80,9 @@ app.get('/deletearticle/:uid/:id', articles.delete)
 // }
 
 
-app.get('*', function(req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
-})
+app.use(express.static('build'));
+app.get('*', function (req, res) {
+  res.sendFile('index.html');
+});
+
 app.listen(port, console.log("Connected on port ", port))
